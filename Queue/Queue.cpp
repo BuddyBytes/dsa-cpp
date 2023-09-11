410e44004422c2cb4ae6c9caf04412c10b88a2cf
OUA LID <117022089+BuddyBytes@users.noreply.github.com>
2023-09-11 14:40:23 +0800

    implement queue dsa

---
+++ Queue/Queue.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+template <size_t StackMaxSize>
+
+class Queue {
+    private:
+    int stackRear;
+    int stackFront;
+    array<int , StackMaxSize> stackArray;
+
+
+    public:
+    Queue(){
+        stackRear = -1 ;
+        stackFront = -1 ;
+        stackArray.fill(0);
+    }
+
+
+    // check if the stack is empty for avoiding underflow error
+    bool isEmpty(){
+        // we need to check if the read-end equal front-end
+        return stackRear == stackFront == -1 ;
+    }
+
+    // check if the stack is full for avoiding overflow error
+    bool isfull(){
+        // we need to check if the rear-end equal the size of stack
+        return stackRear == StackMaxSize - 1;
+    }
+
+    // implements adding to stack function is called as : Enqueue
+    void enqueue(int value){
+         /*
+            for adding a value into stack we need to check in 3 steps
+
+            check if the stack is full for avoiding overflow
+            check if the stack is empty : we need to move the front-end and rear-end to the next index and store the value
+            else :
+            just increament the index value of rear-end and store the value
+          */
+
+         if (isfull()){
+             throw runtime_error("stack overflow");
+         }
+         else if(isEmpty()){
+             stackRear = stackFront = 0 ;
+         }
+         else {
+             stackRear++;
+         }
+         stackArray[stackRear] = value;
+    }
+
+
+    // impelemnts remove from queue function
+    void dequeue(){ // remove elements from queue
+        /*
+            for  removing element from queue we need 3 conditions
+
+            check if queue is not empty for avoiding stack underflow
+            check if rear-end and front-end had the same value and making them both -1
+            else
+            increament the front-end value and make the previus value hold null or 0
+         */
+
+        if(isEmpty()){
+            throw runtime_error("stack underflow");
+        }
+        else if(stackFront == stackRear){
+            stackRear = stackFront = -1 ;
+        }
+        else {
+            stackArray[stackFront] = 0 ;
+            stackFront++;
+        }
+    }
+
+    int countQueueElement(){
+        return ((stackRear - stackFront) + 1 );
+    }
+    void displayQueueElements(){
+        for (int i = static_cast<int>(StackMaxSize) - 1; i >=0 ; i--) {
+            cout << to_string(stackArray[i]) << endl ;
+        }
+    }
+};
+
+int main(){
+    Queue<3> queue;
+    queue.enqueue(5);
+    queue.enqueue(4);
+    queue.enqueue(3);
+    queue.displayQueueElements();
+    cout << to_string(queue.countQueueElement()) << endl ;
+    return 0;
+}
